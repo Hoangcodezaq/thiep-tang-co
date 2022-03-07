bca55fc370c65a01b79a80eec2e0cf6369223c45
Hoangcodezaq <101127326+Hoangcodezaq@users.noreply.github.com>
2022-03-08 00:02:26 +0700

    Add files via upload
    adc

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Phụ nữ VN';
+const giftUrl = '';
+const eventName = 'Chúc Mừng 8/3';
+const titleCard = 'Tặng cô chủ nghiệm của em';
+const contentCard = 'Wishing a very Happy International Women’s Day to the woman who has contributed the most to shape my life. (Chúc cô ngày quốc tế phụ nữ vui vẻ, hạnh phúc - người lái đò thân yêu đóng vai trò quan trọng trong việc giúp em định hình cuộc đời mình).';
+
+
+const giftImage = 'anhco.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
